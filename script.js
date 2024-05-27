1a553d8c52b72c2c3d8d05bf667577286467bf35
RodrigoFariassilva <166635869+RodrigoFariassilva@users.noreply.github.com>
2024-05-27 12:13:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -16,34 +16,4 @@ document.addEventListener("DOMContentLoaded", function() {
     }, 5000); 
     // Altera a cada 5 segundos (5.000 milissegundos)
   });
-  //Botão de cadastro ou criar conta de usuario//
-  document.addEventListener('DOMContentLoaded', (event) => {
-    const botao = document.getElementById('botaouser');
-
-    botao.addEventListener('click', () => {
-        window.location.href = 'outra_pagina.html'; // substitua com o caminho da sua página de destino
-    });
-});
 //Carrossel de Imagens//
-let currentIndex = 0;
-
-function showSlide(index) {
-    const slides = document.querySelectorAll('.carousel-item');
-    if (index >= slides.length) {
-        currentIndex = 0;
-    } else if (index < 0) {
-        currentIndex = slides.length - 1;
-    } else {
-        currentIndex = index;
-    }
-    const offset = -currentIndex * 100;
-    document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
-}
-
-function nextSlide() {
-    showSlide(currentIndex + 1);
-}
-
-function prevSlide() {
-    showSlide(currentIndex - 1);
-}
